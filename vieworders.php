76dcdb399c015ea8cfe4f1055c841ba0d7fea70f
gololol <gololol@ukr.net>
2015-05-08 12:29:31 +0400

    a

--- vieworders.php
+++ vieworders.php
@@ -17,15 +17,28 @@ $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
     <h2>Просмотр заказов от клиентов</h2>
 
 <?php
-    @ $fp = fopen("$DOCUMENT_ROOT/orders/orders.txt",'rb');
+    @ $fp = fopen("$DOCUMENT_ROOT/orders.txt",'rb');
     if (!$fp){
         echo "<p><strong>Нет обработанных заказов. Загляните позже.</strong></p>";
         exit;
     }
-
     while (!feof($fp)){
         $order = fgets($fp, 999);
         echo $order."<br />";
+    }
+    echo "<p> \$DOCUMENT_ROOT: $DOCUMENT_ROOT</p>";
+    echo "<p> \$DOCUMENT_ROOT: ".$DOCUMENT_ROOT."</p>";
+    fclose($fp);
+    //
+    // использовать функцию fgetc();
+    echo "<p>использовать функцию fgetc()</p>";
+    echo "<br />";
+    @ $fp = fopen("$DOCUMENT_ROOT/orders/orders.txt",'rb');
+    while (!feof($fp)) {
+        $char = fgetc($fp);
+        if (!feof($fp)) {
+            if($char=="\n"or$char=="\t"){if($char=="\n"){echo "<br /><br />";}else{echo "<br />";}}else{echo $char;}
+        }
     }
     fclose($fp);
 ?>
